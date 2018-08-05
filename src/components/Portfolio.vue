9a2d276cf257ee9b367ce5b8401ae4dc58767000
Nguyen_Thanh_Trung <nt.naisha.trung@gmail.com>
2018-08-05 22:42:39 +0700

    update

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -262,7 +262,7 @@
                     <div class="row">
                         <div class="col-md-12">
                             <h2 class="text-color-quaternary text-uppercase font-weight-extra-bold m-none text-left">Education</h2>
-                            <div class="owl-carousel nav-bottom custom-carousel-style-1 custom-item-padding custom-dots-style-1" data-plugin-options="{'items': 1, 'loop': false, 'dots': true, 'nav': false}">
+                            <div class="owl-carousel nav-bottom custom-carousel-style-1 custom-item-padding custom-dots-style-1 text-center" data-plugin-options="{'items': 1, 'loop': false, 'dots': true, 'nav': false}">
                                 <div v-for="item in user.educations" :key="user.educations.indexOf(item)">
                                     <div class="col-md-12 col-sm-12">
                                         <div class="custom-box-details-2 background-color-light custom-box-shadow-3">
